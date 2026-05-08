4b3bff0e00fe22f17255828a1336a1bb4bbc8828
greengooscenario <pggs@posteo.de>
2026-05-08 15:31:46 +0200

    first attempts at fetching mail

--- autopin.py
+++ autopin.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 
 # autopin main file
+# ###################
+
+# imports
 
 from configparser import ConfigParser
-
+import imaplib
+import sys
 
 
 # main program logic
 
-myConfig = ConfigParser()
+creds = ConfigParser()
+creds.read("credentials.ini")
 
-myConfig.read("autopin.ini")
+imapserver = creds.get("Credentials", "imapserver")
+username = creds.get("Credentials", "username")
+password = creds.get("Credentials", "password")
+
+print(username)
+
+##here will come the main loop
+
+# instantiate an IMAP4 class with SSL
+
+imap = imaplib.IMAP4_SSL(imapserver)
+print(imap.login(username, password))
+print(imap.list())
+
